9b5dbfffdd9019490d1ad836964ebef7cdb0a2ef
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-06-06 12:43:41 -0300

    init: Commit inicial do projeto-fundamentos.

---
+++ projeto-fundamentos/dados/cursos.php
@@ -0,0 +1,24 @@
+<?php
+
+function cursos() {
+    return [
+        "PHP" => [
+            "nome_curso" => "Curso de PHP Fundamentos",
+            "versao_ferramenta" => 7.4,
+            "carga_horaria" => 40,
+            "status" => true,
+        ],
+        "Java" => [
+            "nome_curso" => "Curso de Java Fundamentos",
+            "versao_ferramenta" => 11.4,
+            "carga_horaria" => 30,
+            "status" => false,
+        ],
+        "C#" => [
+            "nome_curso" => "Curso de C# Fundamentos",
+            "versao_ferramenta" => 5,
+            "carga_horaria" => 60,
+            "status" => true,
+        ]
+    ];
+}
