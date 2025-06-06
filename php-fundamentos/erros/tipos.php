0191f3e0b1da6e434fc6fe15b276b7796698b427
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-06-06 10:11:25 -0300

    feat: adicionar conteúdo sobre erros e exceções.

---
+++ php-fundamentos/erros/tipos.php
@@ -0,0 +1,24 @@
+<?php
+
+error_reporting(E_ALL);
+
+// Notice - Não para a execução
+// $valor1 = 10;
+// echo $valor;
+
+// Warning - Não para a execução do programa
+// $numero = NAO_EXISTE;
+
+// Fatal - Para a execução
+// function somar ($valor1, $valor2) {
+//     return $valor2 + $valor1;
+// }
+
+// function somar ($valor1, $valor2) {
+//     return $valor2 + $valor1;
+// }
+
+// Parse error
+// echo "Mensagem sem ponto e vírgula no fim da linha"
+
+echo "Chegou ao fim do programa";
