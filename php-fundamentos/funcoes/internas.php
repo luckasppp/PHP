30bb9899a8d61426db78fc7a5c3c13d159d661f2
Lucas Damasceno <damasceno_lucas@outlook.com>
2025-06-06 09:43:21 -0300

    feat: Adicionar conteúdo sobre Funções.

---
+++ php-fundamentos/funcoes/internas.php
@@ -0,0 +1,10 @@
+<?php
+
+echo strtolower("TReina WeB"); // deixa tudo minúsculo.
+echo "<br>";
+
+echo strtoupper("TReina WeB"); // deixa tudo minúsculo.
+echo "<br>";
+
+echo str_replace("Z", "e", "TrZina WZb"); // deixa tudo minúsculo.
+echo "<br>";
---
+++ php-fundamentos/funcoes/personalizadas.php
@@ -0,0 +1,16 @@
+<?php
+
+function somar($valor1, $valor2)
+{
+    return $valor1 + $valor2;
+}
+
+$soma = somar(12, 10);
+
+echo $soma;
+echo "<br>";
+
+$soma2 = somar(5, 8);
+
+echo $soma2;
+echo "<br>";
